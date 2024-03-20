e02032f8df22c6dab554a9703115ad2c0a9fd2f0
tolyan-wsl <tolyan@wsl.sctb28.sctbelpa>
2024-03-20 13:06:50 +0300

    +x

--- pre.sh
+++ pre.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-apt-get update
-apt-get install -y dotnet
-dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+echo "Installing .NET Core SDK"
+
+
+#sudo apt-get update
+#sudo apt-get install -y dotnet
+#sudo dotnet tool install -g Microsoft.Web.LibraryManager.Cli
